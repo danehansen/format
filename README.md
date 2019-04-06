5e74f7f7e9d3925843026d13ba91b4bc9d73c3f2
Dane Hansen <dane@danehansen.com>
2019-04-05 18:43:26 -0700

    use package-helper

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# format
+# format ![npm bundle size (scoped)](https://img.shields.io/bundlephobia/minzip/@danehansen/format.svg) ![npm](https://img.shields.io/npm/dt/@danehansen/format.svg)
 
 The format library contains a collection of formatting methods either written or collected to make my life easier. These methods are not validators, they all work under the assumption that the input is valid already.
 
@@ -26,23 +26,23 @@ In your browser:
 
 ## Methods
 
-* __creditCard__(str:String, secure:* = '*'):String  
-Accepts a valid 15-16 digit credit card number and puts the spaces in appropriately. Optionally will replace all digits except the last 4 with another character.
-* __dollars__(amount:Float, showCents:Boolean = true):String  
-Turns a float into a properly formatted string with commas and dollar sign.
-* __expiration__(str:String):String  
-Formats a month/year date as MM/YY.
-* __phoneNumber__(str:String, delimiter:String):String  
-Formats a phone number as 1 (234) 567-8910 by default or else with an optional delimiter like 123.456.7891
-* __prepend__(str:String, len:uint, char:String = '0'):String  
-Prepends a string with a character until it reaches a minimum length.
-* __seperateThousands__(str:*):String  
-Formats a number or string like 1,234,567,890.
-* __time__(h:uint, m:uint, s:uint, ms:uint, delimiter:String = ':'):String  
-Formats a time like 01:02:03:004. If hours or milliseconds are not supplied, they will be left off the string.
-* __toTitleCase__(str:String):String  
-Formats string into title case.
-* __unicode__(str:String):String  
-Converts a string to unicode.
-* __zipCode__(str:String, country:String = 'us'):String  
-Formats a US zipcode like 12345-6789 or a Canadian one like A1B 2C3.
+- **creditCard**(str:String, secure:_ = '_'):String  
+  Accepts a valid 15-16 digit credit card number and puts the spaces in appropriately. Optionally will replace all digits except the last 4 with another character.
+- **dollars**(amount:Float, showCents:Boolean = true):String  
+  Turns a float into a properly formatted string with commas and dollar sign.
+- **expiration**(str:String):String  
+  Formats a month/year date as MM/YY.
+- **phoneNumber**(str:String, delimiter:String):String  
+  Formats a phone number as 1 (234) 567-8910 by default or else with an optional delimiter like 123.456.7891
+- **prepend**(str:String, len:uint, char:String = '0'):String  
+  Prepends a string with a character until it reaches a minimum length.
+- **seperateThousands**(str:\*):String  
+  Formats a number or string like 1,234,567,890.
+- **time**(h:uint, m:uint, s:uint, ms:uint, delimiter:String = ':'):String  
+  Formats a time like 01:02:03:004. If hours or milliseconds are not supplied, they will be left off the string.
+- **toTitleCase**(str:String):String  
+  Formats string into title case.
+- **unicode**(str:String):String  
+  Converts a string to unicode.
+- **zipCode**(str:String, country:String = 'us'):String  
+  Formats a US zipcode like 12345-6789 or a Canadian one like A1B 2C3.
--- test/test.js
+++ test/test.js
@@ -1,242 +1,290 @@
-import * as format from '../src/format'
-import { expect } from 'chai'
+import * as format from "../src/format";
+import { expect } from "chai";
+import fs from "fs";
+import path from "path";
 
-const REPEAT = 100
+const REPEAT = 100;
 
 function random(limitA = 1, limitB = 0, round = false, choke = 1) {
-  let total = 0
+  let total = 0;
   if (!round) {
-    for(let i = 0; i < choke; i++) {
-      total += Math.random() * ((limitB - limitA) / choke)
-    }
-    return limitA + total
-  } else {
-    const low = Math.ceil(Math.min(limitA, limitB))
-    const high = Math.floor(Math.max(limitA, limitB))
     for (let i = 0; i < choke; i++) {
-      total += Math.random() * ((high + 1 - low) / choke)
+      total += Math.random() * ((limitB - limitA) / choke);
     }
-    return Math.floor(low + total)
+    return limitA + total;
+  } else {
+    const low = Math.ceil(Math.min(limitA, limitB));
+    const high = Math.floor(Math.max(limitA, limitB));
+    for (let i = 0; i < choke; i++) {
+      total += Math.random() * ((high + 1 - low) / choke);
+    }
+    return Math.floor(low + total);
   }
 }
 
-describe('format', function() {
-  describe('creditCard', function() {
-    it('formats a 16 digit securely with default delimiter', function() {
-      expect(format.creditCard('x41111111111111 11')).to.equal('**** **** **** 1111')
-    })
+describe("format", function() {
+  describe("danehansen-format.min.js", function() {
+    it("is minified", function() {
+      const min = fs.readFileSync(
+        path.join(__dirname, "../danehansen-format.min.js"),
+        "utf8"
+      );
+      expect(min.match(/\n/g)).to.be.null;
+    });
+  });
 
-    it('formats a 16 digit securely with supplied delimiter', function() {
-      expect(format.creditCard('41x11111111111111 ', '-')).to.equal('---- ---- ---- 1111')
-    })
+  describe("creditCard", function() {
+    it("formats a 16 digit securely with default delimiter", function() {
+      expect(format.creditCard("x41111111111111 11")).to.equal(
+        "**** **** **** 1111"
+      );
+    });
 
-    it('formats a 16 digit insecurely', function() {
-      expect(format.creditCard('41.11111111111111 x', false)).to.equal('4111 1111 1111 1111')
-    })
+    it("formats a 16 digit securely with supplied delimiter", function() {
+      expect(format.creditCard("41x11111111111111 ", "-")).to.equal(
+        "---- ---- ---- 1111"
+      );
+    });
 
-    it('formats a 15 digit securely', function() {
-      expect(format.creditCard('3 78 2x82246310005')).to.equal('**** ****** *0005')
-    })
+    it("formats a 16 digit insecurely", function() {
+      expect(format.creditCard("41.11111111111111 x", false)).to.equal(
+        "4111 1111 1111 1111"
+      );
+    });
 
-    it('formats a X digit securely', function() {
-      expect(format.creditCard('1234567890123456789')).to.equal('***************6789')
-    })
-  })
+    it("formats a 15 digit securely", function() {
+      expect(format.creditCard("3 78 2x82246310005")).to.equal(
+        "**** ****** *0005"
+      );
+    });
 
-  describe('dollars', function() {
-    it('rounds a number into whole dollars', function() {
-      expect(format.dollars(1111.9, false)).to.equal('$1,112')
-    })
+    it("formats a X digit securely", function() {
+      expect(format.creditCard("1234567890123456789")).to.equal(
+        "***************6789"
+      );
+    });
+  });
 
-    it('rounds a string into whole dollars', function() {
-      expect(format.dollars('1111.9', false)).to.equal('$1,112')
-    })
+  describe("dollars", function() {
+    it("rounds a number into whole dollars", function() {
+      expect(format.dollars(1111.9, false)).to.equal("$1,112");
+    });
 
-    it('rounds a number into dollars and cents', function() {
-      expect(format.dollars(-1111.899)).to.equal('-$1,111.90')
-    })
+    it("rounds a string into whole dollars", function() {
+      expect(format.dollars("1111.9", false)).to.equal("$1,112");
+    });
 
-    it('rounds a string into dollars and cents', function() {
-      expect(format.dollars('-1111.899')).to.equal('-$1,111.90')
-    })
-  })
+    it("rounds a number into dollars and cents", function() {
+      expect(format.dollars(-1111.899)).to.equal("-$1,111.90");
+    });
 
-  describe('expiration', function() {
-    it('formats 3 digits', function() {
-      expect(format.expiration('123')).to.equal('01/23')
-    })
+    it("rounds a string into dollars and cents", function() {
+      expect(format.dollars("-1111.899")).to.equal("-$1,111.90");
+    });
+  });
 
-    it('formats 4 digits', function() {
-      expect(format.expiration('1234')).to.equal('12/34')
-    })
+  describe("expiration", function() {
+    it("formats 3 digits", function() {
+      expect(format.expiration("123")).to.equal("01/23");
+    });
 
-    it('formats 5 digits', function() {
-      expect(format.expiration('12345')).to.equal('01/45')
-    })
+    it("formats 4 digits", function() {
+      expect(format.expiration("1234")).to.equal("12/34");
+    });
 
-    it('formats 6 digits', function() {
-      expect(format.expiration('123456')).to.equal('12/56')
-    })
-  })
+    it("formats 5 digits", function() {
+      expect(format.expiration("12345")).to.equal("01/45");
+    });
 
+    it("formats 6 digits", function() {
+      expect(format.expiration("123456")).to.equal("12/56");
+    });
+  });
 
-  describe('phoneNumber', function() {
-    it('uses defined delimiter with country code', function() {
-      expect(format.phoneNumber('12 3a45678910x', '.')).to.equal('1.234.567.8910')
-    })
+  describe("phoneNumber", function() {
+    it("uses defined delimiter with country code", function() {
+      expect(format.phoneNumber("12 3a45678910x", ".")).to.equal(
+        "1.234.567.8910"
+      );
+    });
 
-    it('uses defined delimiter without country code', function() {
-      expect(format.phoneNumber('2 3a45678910x', '.')).to.equal('234.567.8910')
-    })
+    it("uses defined delimiter without country code", function() {
+      expect(format.phoneNumber("2 3a45678910x", ".")).to.equal("234.567.8910");
+    });
 
-    it('uses no delimiter with country code', function() {
-      expect(format.phoneNumber('12 3a45678910x')).to.equal('1 (234) 567-8910')
-    })
+    it("uses no delimiter with country code", function() {
+      expect(format.phoneNumber("12 3a45678910x")).to.equal("1 (234) 567-8910");
+    });
 
-    it('uses no delimiter without country code', function() {
-      expect(format.phoneNumber('2 3a45678910x')).to.equal('(234) 567-8910')
-    })
+    it("uses no delimiter without country code", function() {
+      expect(format.phoneNumber("2 3a45678910x")).to.equal("(234) 567-8910");
+    });
 
-    it('removes non digits from odd length number', function() {
-      expect(format.phoneNumber('12 3a45678910x1112')).to.equal('123456789101112')
-    })
-  })
+    it("removes non digits from odd length number", function() {
+      expect(format.phoneNumber("12 3a45678910x1112")).to.equal(
+        "123456789101112"
+      );
+    });
+  });
 
-  describe('prepend', function() {
-    it('adds zeros by default', function() {
-      expect(format.prepend(1, 5)).to.equal('00001')
-    })
+  describe("prepend", function() {
+    it("adds zeros by default", function() {
+      expect(format.prepend(1, 5)).to.equal("00001");
+    });
 
-    it('adds supplied character', function() {
-      expect(format.prepend('1', 5, '.')).to.equal('....1')
-    })
+    it("adds supplied character", function() {
+      expect(format.prepend("1", 5, ".")).to.equal("....1");
+    });
 
-    it('does nothing when length is sufficient', function() {
-      const str = '12345'
-      expect(format.prepend(str, 5)).to.equal(str)
-    })
-  })
+    it("does nothing when length is sufficient", function() {
+      const str = "12345";
+      expect(format.prepend(str, 5)).to.equal(str);
+    });
+  });
 
-  describe('seperateThousands', function() {
-    it('rounds and commaifies a string', function() {
-      expect(format.seperateThousands('12345678.9')).to.equal('12,345,679')
-    })
+  describe("seperateThousands", function() {
+    it("rounds and commaifies a string", function() {
+      expect(format.seperateThousands("12345678.9")).to.equal("12,345,679");
+    });
 
-    it('rounds and commaifies a number', function() {
-      expect(format.seperateThousands(12345.6789)).to.equal('12,346')
-    })
-  })
+    it("rounds and commaifies a number", function() {
+      expect(format.seperateThousands(12345.6789)).to.equal("12,346");
+    });
+  });
 
-  describe('time', function() {
-    it('formats with custom delimiter', function() {
+  describe("time", function() {
+    it("formats with custom delimiter", function() {
       for (let i = 0; i < REPEAT; i++) {
-        let m = random(0, 120, true)
-        let s = random(0, 120, true)
-        const result = format.time(null, m, s, null, ' - ')
+        let m = random(0, 120, true);
+        let s = random(0, 120, true);
+        const result = format.time(null, m, s, null, " - ");
         while (s >= 60) {
-          s -= 60
-          m++
+          s -= 60;
+          m++;
         }
-        expect(result).to.equal(`${format.prepend(m, 2)} - ${format.prepend(s, 2)}`)
+        expect(result).to.equal(
+          `${format.prepend(m, 2)} - ${format.prepend(s, 2)}`
+        );
       }
-    })
+    });
 
-    it('formats with no hours, no milliseconds', function() {
+    it("formats with no hours, no milliseconds", function() {
       for (let i = 0; i < REPEAT; i++) {
-        let m = random(0, 120, true)
-        let s = random(0, 120, true)
-        const result = format.time(null, m, s)
+        let m = random(0, 120, true);
+        let s = random(0, 120, true);
+        const result = format.time(null, m, s);
         while (s >= 60) {
-          s -= 60
-          m++
+          s -= 60;
+          m++;
         }
-        expect(result).to.equal(`${format.prepend(m, 2)}:${format.prepend(s, 2)}`)
+        expect(result).to.equal(
+          `${format.prepend(m, 2)}:${format.prepend(s, 2)}`
+        );
       }
-    })
+    });
 
-    it('formats with hours, no milliseconds', function() {
+    it("formats with hours, no milliseconds", function() {
       for (let i = 0; i < REPEAT; i++) {
-        let h = random(0, 48, true)
-        let m = random(0, 120, true)
-        let s = random(0, 120, true)
-        const result = format.time(h, m, s)
+        let h = random(0, 48, true);
+        let m = random(0, 120, true);
+        let s = random(0, 120, true);
+        const result = format.time(h, m, s);
         while (s >= 60) {
-          s -= 60
-          m++
+          s -= 60;
+          m++;
         }
         while (m >= 60) {
-          m -= 60
-          h++
+          m -= 60;
+          h++;
         }
-        expect(result).to.equal(`${format.prepend(h, 2)}:${format.prepend(m, 2)}:${format.prepend(s, 2)}`)
+        expect(result).to.equal(
+          `${format.prepend(h, 2)}:${format.prepend(m, 2)}:${format.prepend(
+            s,
+            2
+          )}`
+        );
       }
-    })
+    });
 
-    it('formats with hours and milliseconds', function() {
+    it("formats with hours and milliseconds", function() {
       for (let i = 0; i < REPEAT; i++) {
-        let h = random(0, 48, true)
-        let m = random(0, 120, true)
-        let s = random(0, 120, true)
-        let ms = random(0, 2000, true)
-        const result = format.time(h, m, s, ms)
+        let h = random(0, 48, true);
+        let m = random(0, 120, true);
+        let s = random(0, 120, true);
+        let ms = random(0, 2000, true);
+        const result = format.time(h, m, s, ms);
         while (ms >= 1000) {
-          ms -= 1000
-          s++
+          ms -= 1000;
+          s++;
         }
         while (s >= 60) {
-          s -= 60
-          m++
+          s -= 60;
+          m++;
         }
         while (m >= 60) {
-          m -= 60
-          h++
+          m -= 60;
+          h++;
         }
-        expect(result).to.equal(`${format.prepend(h, 2)}:${format.prepend(m, 2)}:${format.prepend(s, 2)}:${format.prepend(ms, 3)}`)
+        expect(result).to.equal(
+          `${format.prepend(h, 2)}:${format.prepend(m, 2)}:${format.prepend(
+            s,
+            2
+          )}:${format.prepend(ms, 3)}`
+        );
       }
-    })
+    });
 
-    it('formats with no hours and with milliseconds', function() {
+    it("formats with no hours and with milliseconds", function() {
       for (let i = 0; i < REPEAT; i++) {
-        let m = random(0, 120, true)
-        let s = random(0, 120, true)
-        let ms = random(0, 2000, true)
-        const result = format.time(null, m, s, ms)
+        let m = random(0, 120, true);
+        let s = random(0, 120, true);
+        let ms = random(0, 2000, true);
+        const result = format.time(null, m, s, ms);
         while (ms >= 1000) {
-          ms -= 1000
-          s++
+          ms -= 1000;
+          s++;
         }
         while (s >= 60) {
-          s -= 60
-          m++
+          s -= 60;
+          m++;
         }
-        expect(result).to.equal(`${format.prepend(m, 2)}:${format.prepend(s, 2)}:${format.prepend(ms, 3)}`)
+        expect(result).to.equal(
+          `${format.prepend(m, 2)}:${format.prepend(s, 2)}:${format.prepend(
+            ms,
+            3
+          )}`
+        );
       }
-    })
-  })
+    });
+  });
 
-  describe('toTitleCase', function() {
-    it('changes string to title case', function() {
-      expect(format.toTitleCase('aB c d.ef . GhIjK')).to.equal('Ab C D.Ef . Ghijk')
-    })
-  })
+  describe("toTitleCase", function() {
+    it("changes string to title case", function() {
+      expect(format.toTitleCase("aB c d.ef . GhIjK")).to.equal(
+        "Ab C D.Ef . Ghijk"
+      );
+    });
+  });
 
-  describe('unicode', function() {
-    it('changes string to unicode', function() {
-      expect(format.unicode('abc123')).to.equal('&#97;&#98;&#99;&#49;&#50;&#51;')
-    })
-  })
+  describe("unicode", function() {
+    it("changes string to unicode", function() {
+      expect(format.unicode("abc123")).to.equal(
+        "&#97;&#98;&#99;&#49;&#50;&#51;"
+      );
+    });
+  });
 
-  describe('zipCode', function() {
-    it('formats 5 digit US zip codes', function() {
-      expect(format.zipCode('1.23 45a')).to.equal('12345')
-    })
+  describe("zipCode", function() {
+    it("formats 5 digit US zip codes", function() {
+      expect(format.zipCode("1.23 45a")).to.equal("12345");
+    });
 
-    it('formats 9 digit US zip codes', function() {
-      expect(format.zipCode('123 45.a6-78 9')).to.equal('12345-6789')
-    })
+    it("formats 9 digit US zip codes", function() {
+      expect(format.zipCode("123 45.a6-78 9")).to.equal("12345-6789");
+    });
 
-    it('formats candadian zip codes', function() {
-      expect(format.zipCode('a1a1a1b2', 'ca')).to.equal('A1A 1A1')
-    })
-  })
-})
+    it("formats candadian zip codes", function() {
+      expect(format.zipCode("a1a1a1b2", "ca")).to.equal("A1A 1A1");
+    });
+  });
+});
